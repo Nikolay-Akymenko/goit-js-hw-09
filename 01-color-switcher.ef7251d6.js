79dc9fd034adddea1338ca540d8aa04ff5305c44
Nikolay-Akymenko <103488690+Nikolay-Akymenko@users.noreply.github.com>
2022-09-08 20:31:59 +0000

    Deploying to gh-pages from @ Nikolay-Akymenko/goit-js-hw-09@c766185d5f24435e1eb95992e8887a566239db6d 🚀

--- 01-color-switcher.ef7251d6.js
+++
@@ -1,2 +0,0 @@
-const o=new Date;console.log(o);
-//# sourceMappingURL=01-color-switcher.ef7251d6.js.map
--- 01-color-switcher.f9fe0c24.js
+++
@@ -1,2 +0,0 @@
-!function(){var n=new Date;console.log(n)}();
-//# sourceMappingURL=01-color-switcher.f9fe0c24.js.map
