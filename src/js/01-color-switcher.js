9a056403da842d0f99ff8b94b5dd8c51b07bd1ff
Nikolay-Akymenko <nikolay0939@gmail.com>
2022-09-11 19:44:52 +0300

    Задание 1  color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
 
+let timerId = null;
+
+startBtn.addEventListener('click', onStartColorSwitcher);
+stopBtn.addEventListener('click', onStopColorSwitcher);
+
+function onStartColorSwitcher() {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    
+    timerId = setInterval(bodyColorSwitcher, 1000);
+    
+    bodyColorSwitcher();
+    console.log('start: change color');
+};
+
+function onStopColorSwitcher() {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+
+    clearInterval(timerId);
+    console.log('stop: change color')
+};
+
+function bodyColorSwitcher() {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
