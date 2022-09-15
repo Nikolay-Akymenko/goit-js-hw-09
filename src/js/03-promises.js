538a30b4ff96e969b09b572a14d889939f9c46bf
Nikolay-Akymenko <nikolay0939@gmail.com>
2022-09-15 11:42:27 +0300

    bugfix promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -20,11 +20,9 @@ function onSubmitForm(event) {
   for (let position = 1; position <= amount; position += 1) {
     createPromise(position, delay)
       .then(({ position, delay }) => {
-        setTimeout(() => {
           Notiflix.Notify.success(
             `✅ Fulfilled promise ${position} in ${delay}ms`
           );
-        }, delay);
       })
       .catch(({ position, delay }) => {
         setTimeout(() => {
@@ -39,11 +37,13 @@ function onSubmitForm(event) {
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-      resolve({ position, delay })
-    } else {
-      reject({ position, delay });
-    }
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay })
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
   });
 }
