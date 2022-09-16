0dd3301ea1539607bb68b8097609b125215ca60c
Nikolay-Akymenko <nikolay0939@gmail.com>
2022-09-16 16:55:46 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,21 +1,24 @@
 import Notiflix from 'notiflix';
 
+
 const inputForm = document.querySelector('.form');
-inputForm.addEventListener('input', onInputForm);
+// inputForm.addEventListener('input', onInputForm);
 inputForm.addEventListener('submit', onSubmitForm);
 
-let delay = 0;
-let step = 0;
-let amount = 0;
+// let delay = 0;
+// let step = 0;
+// let amount = 0;
 
-function onInputForm(event) {
-  delay = Number(event.currentTarget.delay.value);
-  step = Number(event.currentTarget.step.value);
-  amount = Number(event.currentTarget.amount.value);
-};
+// function onInputForm(event) {
+
+// };
 
 function onSubmitForm(event) {
   event.preventDefault();
+
+ let delay = Number(event.currentTarget.delay.value);
+ let step = Number(event.currentTarget.step.value);
+ let amount = Number(event.currentTarget.amount.value);
 
   for (let position = 1; position <= amount; position += 1) {
     createPromise(position, delay)
@@ -25,15 +28,13 @@ function onSubmitForm(event) {
           );
       })
       .catch(({ position, delay }) => {
-        setTimeout(() => {
           Notiflix.Notify.failure(
             `❌ Rejected promise ${position} in ${delay}ms`
           );
-        }, delay);
       });
     delay += step;
   }
-};
+}
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
